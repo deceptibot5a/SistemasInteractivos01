318202020fd156e9a8c223dd0ec6cfe12d9a95ad
deceptibot5a <deceptibot5a@gmail.com>
2023-02-19 22:01:30 -0500

    Leaderboard

--- Sistemas01/Assets/Scripts/HttpAuthHandler.cs
+++ Sistemas01/Assets/Scripts/HttpAuthHandler.cs
@@ -11,14 +11,17 @@ public class HttpAuthHandler : MonoBehaviour
     [SerializeField] private string ServerApiURL;
     [SerializeField] List<GameObject> objectosLogin = new List<GameObject>();
     [SerializeField] List<GameObject> objectosUsuario = new List<GameObject>();
+    [SerializeField] TMP_Text userText;
+    [SerializeField] TMP_Text scoreText;
+    List<User> listaUsuarios = new List<User>();
+    [SerializeField] List<TMP_Text> listaPuntajes = new List<TMP_Text>();
+    [SerializeField] List<TMP_Text> listaNombres = new List<TMP_Text>();
+    List<int> valoresPuntajes = new List<int>(5);
 
     public string Token { get; set; }
     public string Username { get; set; }
 
     void Start() {
-        List<User> listaUsuarios = new List<User>();
-        var listaUsuariosOrdenada = listaUsuarios.OrderByDescending(u => u.data.score).ToList<User>();
-
         Token = PlayerPrefs.GetString("token");
         Username = PlayerPrefs.GetString("username");
 
@@ -30,13 +33,20 @@ public class HttpAuthHandler : MonoBehaviour
             Debug.Log(Username);
             StartCoroutine(GetPerfil());
         }
-    }
+    }/*
+    void Update() {
+        //var listaUsuariosOrdenada = listaUsuarios.OrderByDescending(user => user.data.score).ToList<User>();
+        for (int i = 0; i < listaUsuarios.Count; i++) {
+            //listaPuntajes[i].text = 
+                //(i + 1) + " - " + listaUsuarios[i].username + " - " + listaUsuarios[i].data.score;
+        }
+    }*/
     public void Registrar() {
         User user = new User();
         user.username = GameObject.Find("InputUsername").GetComponent<TMP_InputField>().text;
         user.password = GameObject.Find("InputPassword").GetComponent<TMP_InputField>().text;
-        //user.data.score = "0";
         string postData = JsonUtility.ToJson(user);
+        listaUsuarios.Add(user);
         StartCoroutine(Registro(postData));
     }
 
@@ -45,11 +55,11 @@ public class HttpAuthHandler : MonoBehaviour
         user.username = GameObject.Find("InputUsername").GetComponent<TMP_InputField>().text;
         user.password = GameObject.Find("InputPassword").GetComponent<TMP_InputField>().text;
         string postData = JsonUtility.ToJson(user);
+        listaUsuarios.Add(user);
         StartCoroutine(Login(postData));
     }
     
     IEnumerator Registro(string postData) {
-
         UnityWebRequest www = UnityWebRequest.Put(ServerApiURL + "/api/usuarios",postData);
         www.method = "POST";
         www.SetRequestHeader("Content-Type", "application/json");
@@ -65,8 +75,7 @@ public class HttpAuthHandler : MonoBehaviour
                 AuthJsonData jsonData = JsonUtility.FromJson<AuthJsonData>(www.downloadHandler.text);
 
                 Debug.Log(jsonData.usuario.username + " se regitro con id " + jsonData.usuario._id);
-                //Proceso de autenticacion
-                LoginScreen();
+                LoginScreen(); //Proceso de autenticacion
             } else {
                 string mensaje = "Status :" + www.responseCode;
                 mensaje += "\ncontent-type:" + www.GetResponseHeader("content-type");
@@ -121,8 +130,7 @@ public class HttpAuthHandler : MonoBehaviour
                 AuthJsonData jsonData = JsonUtility.FromJson<AuthJsonData>(www.downloadHandler.text);
 
                 Debug.Log(jsonData.usuario.username + " Sigue con la sesion inciada");
-                //Cambiar de escena
-                LoginScreen();
+                LoginScreen(); //Cambiar de pantalla
             } else {
                 string mensaje = "Status :" + www.responseCode;
                 mensaje += "\ncontent-type:" + www.GetResponseHeader("content-type");
@@ -138,11 +146,15 @@ public class HttpAuthHandler : MonoBehaviour
         for (int i = 0; i < objectosUsuario.Count; i++) {
             objectosUsuario[i].SetActive(true);
         }
+        userText.text = "Welcome, " + Username;
+        for (int i = 0; i < listaNombres.Count; i++) {
+            if (listaNombres[i].text == "Username") {
+                listaNombres[i].text = Username;
+                break;
+            }
+        }
     }
     public void Logout() {
-        Token = null;
-        Debug.Log("Funciono");
-        Debug.Log(Token);
         for (int i = 0; i < objectosUsuario.Count; i++) {
             objectosUsuario[i].SetActive(false);
         }
@@ -166,9 +178,37 @@ public class HttpAuthHandler : MonoBehaviour
 
             if (www.responseCode == 200) {
                 UserData myData = JsonUtility.FromJson<UserData>(www.downloadHandler.text);
-                myData.score = GameObject.Find("InputScore").GetComponent<TMP_InputField>().text;
                 if (myData.score == null) {
                     myData.score = "0";
+                }
+                myData.score = GameObject.Find("InputScore").GetComponent<TMP_InputField>().text;
+                scoreText.text = "Your score is: " + myData.score;
+                for (int i = 0; i < listaPuntajes.Count; i++) {
+                    if (listaNombres[i].text == Username) {
+                        listaPuntajes[i].text = myData.score;
+
+                        valoresPuntajes[i] = int.Parse(listaPuntajes[i].text);
+                    }
+                }
+                TMP_Text temp;
+                int temp2;
+                
+                for (int i = 0; i < listaPuntajes.Count; i++) {
+                    for (int j = 0; j < listaPuntajes.Count - 1; j++) {
+                        if (valoresPuntajes[j] < valoresPuntajes[j + 1]) {
+                            temp = listaPuntajes[j];
+                            listaPuntajes[j] = listaPuntajes[j + 1];
+                            listaPuntajes[j + 1] = temp;
+
+                            temp = listaNombres[j];
+                            listaNombres[j] = listaNombres[j + 1];
+                            listaNombres[j + 1] = temp;
+
+                            temp2 = valoresPuntajes[j];
+                            valoresPuntajes[j] = valoresPuntajes[j + 1];
+                            valoresPuntajes[j + 1] = temp2;
+                        }
+                    }
                 }
                 Debug.Log(myData.score);
             } else {
@@ -190,10 +230,9 @@ public class User {
 
     public User() {
     }
-    public User(string username, string password, string data) {
+    public User(string username, string password) {
         this.username = username;
         this.password = password;
-        this.data.score = data;
     }
 }
 public class UserData {
